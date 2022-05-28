3557a1afd5a3c9cc761217a9f5270f8ee65dce98
osmanforhad <developer.osmanforhad@gmail.com>
2022-05-29 01:11:47 +0600

    project setup

---
+++ src/main/java/net/osmanforhad/main/SpringBootEmpApiApplication.java
@@ -0,0 +1,13 @@
+package net.osmanforhad.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmpApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/osmanforhad/main/SpringBootEmpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.osmanforhad.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
