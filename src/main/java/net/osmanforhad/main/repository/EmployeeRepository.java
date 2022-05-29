a915a53270383a072675732d77970310bf9b48df
osmanforhad <developer.osmanforhad@gmail.com>
2022-05-29 14:29:01 +0600

    setup JPA Repository interface class

---
+++ src/main/java/net/osmanforhad/main/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package net.osmanforhad.main.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.osmanforhad.main.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
